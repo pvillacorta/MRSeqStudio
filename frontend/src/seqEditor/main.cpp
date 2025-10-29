e82ed593520f8fb96e879cf5748ffc1d1cc86aee
Pablo Villacorta <pablo.villacorta@uva.es>
2025-10-29 13:20:57 +0100

    Improved compatibility with Qt 6.10

--- frontend/src/seqEditor/main.cpp
+++ frontend/src/seqEditor/main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QQuickStyle>
+#include <QString>
 
 #include "backend.h"
 
@@ -10,11 +11,13 @@ int main(int argc, char *argv[])
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    using namespace Qt::Literals::StringLiterals;
+
     QApplication app(argc, argv);
 
     QQuickStyle::setStyle("Basic");
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/qml/Main.qml"_qs);
+    const QUrl url(u"qrc:/qml/Main.qml"_s);
 
     // Creation of an instance of the class Backend
     Backend backend;
